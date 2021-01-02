76370eaa5a3fd4efb180f356554691b29cfda612
React CAD Dev <react.cad.dev@gmail.com>
2021-01-02 17:48:45 +0000

    Watch wasm source

--- packages/storybook-addon/src/ReactCadPreview.tsx
+++ packages/storybook-addon/src/ReactCadPreview.tsx
@@ -1,51 +1,94 @@
+/// <reference types="resize-observer-browser" />
+//
 import React from "react";
-import createPreviewWindow, {
-  ReactCadCoreModule
-} from "@react-cad/core";
+import createPreviewWindow, { ReactCadCoreModule } from "@react-cad/core";
 import ReactCadRenderer from "@react-cad/renderer";
 
 interface Props {
+  className?: string;
   shape: React.ReactElement<unknown>;
 }
 
-const ReactCadPreview: React.FC<Props> = ({ shape }) => {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+const ReactCadPreview = React.forwardRef<HTMLDivElement | undefined, Props>(
+  ({ className, shape }, forwardedRef) => {
+    const wrapperRef = React.useRef<HTMLDivElement>(null);
+    const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
-  const [
-    previewWindow,
-    setPreviewWindow
-  ] = React.useState<ReactCadCoreModule>();
+    React.useImperativeHandle(
+      forwardedRef,
+      () => wrapperRef.current ?? undefined
+    );
 
-  React.useEffect(() => {
-    if (canvasRef.current) {
-      createPreviewWindow({
-        print: console.log,
-        printErr: console.warn,
-        canvas: canvasRef.current
-      }).then(setPreviewWindow);
-    }
-  }, []);
+    const [
+      previewWindow,
+      setPreviewWindow
+    ] = React.useState<ReactCadCoreModule>();
 
-  React.useEffect(() => {
-    if (previewWindow) {
-      ReactCadRenderer.render(shape, previewWindow, console.log);
-    }
-  }, [previewWindow, shape]);
+    const [{ width, height }, setDimensions] = React.useState({
+      width: 640,
+      height: 640
+    });
 
-  const width = 640;
-  const height = 480;
+    /*
+    React.useLayoutEffect(() => {
+      if (wrapperRef.current) {
+        setDimensions({
+          width: wrapperRef.current.clientWidth,
+          height: wrapperRef.current.clientHeight
+        });
 
-  return (
-    <canvas
-      style={{
-        width: `${width}px`,
-        height: `${height}px`
-      }}
-      width={width * window.devicePixelRatio}
-      height={height * window.devicePixelRatio}
-      ref={canvasRef}
-    />
-  );
-};
+        if ("ResizeObserver" in window) {
+          const resizeObserver = new ResizeObserver(entries => {
+            for (let entry of entries) {
+              setDimensions({
+                width: entry.contentRect.width,
+                height: entry.contentRect.height
+              });
+            }
+          });
+
+          const wrapper = wrapperRef.current;
+          resizeObserver.observe(wrapper);
+          return () => resizeObserver.unobserve(wrapper);
+        }
+      }
+    }, []);
+     */
+
+    React.useEffect(() => {
+      if (canvasRef.current) {
+        createPreviewWindow({
+          print: console.log,
+          printErr: console.warn,
+          canvas: canvasRef.current
+        }).then(setPreviewWindow);
+      }
+    }, []);
+
+    React.useEffect(() => {
+      if (previewWindow) {
+        ReactCadRenderer.render(shape, previewWindow, () => {});
+      }
+    }, [previewWindow, shape]);
+
+    return (
+      <div
+        className={className}
+        ref={wrapperRef}
+      >
+        <canvas
+          style={{
+            width: `${width}px`,
+            height: `${height}px`,
+            display: "block"
+          }}
+          width={width * window.devicePixelRatio}
+          height={height * window.devicePixelRatio}
+          ref={canvasRef}
+        />
+      </div>
+    );
+  }
+);
 
 export default ReactCadPreview;
