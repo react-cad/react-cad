95ddee0e1d24aab8730391a9964203526c2321be
React CAD Dev <react.cad.dev@gmail.com>
2021-01-16 13:35:04 +0000

    Add action to build wasm

--- packages/core/src/main.cpp
+++ packages/core/src/main.cpp
@@ -10,100 +10,113 @@
 #include <OSD_Parallel.hxx>
 
 #include <AIS_Shape.hxx>
-#include <BRepTools.hxx>
-#include <BRep_Builder.hxx>
+#include <BRepAlgoAPI_BuilderAlgo.hxx>
+#include <BRepAlgoAPI_Common.hxx>
+#include <BRepAlgoAPI_Cut.hxx>
+#include <BRepBuilderAPI_Copy.hxx>
+#include <BRepBuilderAPI_Transform.hxx>
 #include <BRepPrimAPI_MakeBox.hxx>
 #include <BRepPrimAPI_MakeCylinder.hxx>
 #include <BRepPrimAPI_MakeSphere.hxx>
 #include <BRepPrimAPI_MakeTorus.hxx>
-#include <BRepBuilderAPI_Transform.hxx>
-#include <BRepBuilderAPI_Copy.hxx>
-#include <BRepAlgoAPI_BuilderAlgo.hxx>
-#include <BRepAlgoAPI_Cut.hxx>
-#include <BRepAlgoAPI_Common.hxx>
-#include <TopoDS_Shape.hxx>
-#include <TopTools_ListOfShape.hxx>
+#include <BRepTools.hxx>
+#include <BRep_Builder.hxx>
 #include <Standard_ArrayStreamBuffer.hxx>
+#include <TopTools_ListOfShape.hxx>
+#include <TopoDS_Shape.hxx>
 
-#include <gp_Trsf.hxx>
 #include <gp.hxx>
+#include <gp_Trsf.hxx>
 
 #include <emscripten.h>
-#include <emscripten/html5.h>
 #include <emscripten/bind.h>
+#include <emscripten/html5.h>
 
 //! Global viewer instance.
 static WasmOcctView aViewer;
 static Handle(AIS_Shape) aShapePrs;
 
-void clearShape() {
+void clearShape()
+{
   AIS_ListOfInteractive aShapes;
-  aViewer.Context()->DisplayedObjects (AIS_KOI_Shape, -1, aShapes);
-  for (AIS_ListOfInteractive::Iterator aShapeIter (aShapes); aShapeIter.More(); aShapeIter.Next())
+  aViewer.Context()->DisplayedObjects(AIS_KOI_Shape, -1, aShapes);
+  for (AIS_ListOfInteractive::Iterator aShapeIter(aShapes); aShapeIter.More(); aShapeIter.Next())
   {
-    aViewer.Context()->Remove (aShapeIter.Value(), false);
+    aViewer.Context()->Remove(aShapeIter.Value(), false);
   }
   aViewer.View()->Redraw();
 }
 
-void setShape(TopoDS_Shape& aShape) {
+void setShape(TopoDS_Shape &aShape)
+{
   clearShape();
 
   aShapePrs->SetShape(aShape);
-  aShapePrs->SetMaterial (Graphic3d_NameOfMaterial_Silver);
-  aViewer.Context()->Display (aShapePrs, AIS_Shaded, 0, false);
+  aShapePrs->SetMaterial(Graphic3d_NameOfMaterial_Silver);
+  aViewer.Context()->Display(aShapePrs, AIS_Shaded, 0, false);
   aViewer.View()->Redraw();
 }
 
-void fitShape() {
-  aViewer.View()->FitAll (0.1, false);
+void fitShape()
+{
+  aViewer.View()->FitAll(0.1, false);
   aViewer.View()->Redraw();
 }
 
-TopoDS_Shape copyShape(TopoDS_Shape& aShape) {
+TopoDS_Shape copyShape(TopoDS_Shape &aShape)
+{
   return BRepBuilderAPI_Copy(aShape);
 }
 
-TopoDS_Shape makeBox(double x, double y, double z) {
+TopoDS_Shape makeBox(double x, double y, double z)
+{
   BRepPrimAPI_MakeBox aBox(x, y, z);
-  return aBox.Solid(); 
+  return aBox.Solid();
 }
 
-TopoDS_Shape makeCylinder(double radius, double height) {
+TopoDS_Shape makeCylinder(double radius, double height)
+{
   BRepPrimAPI_MakeCylinder aCylinder(radius, height);
-  return aCylinder.Solid(); 
+  return aCylinder.Solid();
 }
 
-TopoDS_Shape makeSphere(double radius) {
+TopoDS_Shape makeSphere(double radius)
+{
   BRepPrimAPI_MakeSphere aSphere(radius);
-  return aSphere.Solid(); 
+  return aSphere.Solid();
 }
 
-TopoDS_Shape makeTorus(double radius1, double radius2) {
+TopoDS_Shape makeTorus(double radius1, double radius2)
+{
   BRepPrimAPI_MakeTorus aTorus(radius1, radius2);
-  return aTorus.Solid(); 
+  return aTorus.Solid();
 }
 
-gp_Trsf makeRotation(const gp_Ax1& anAxis, Standard_Real angle) {
+gp_Trsf makeRotation(const gp_Ax1 &anAxis, Standard_Real angle)
+{
   gp_Trsf trsf;
   trsf.SetRotation(anAxis, angle);
   return trsf;
 }
 
-gp_Trsf makeTranslation(Standard_Real x, Standard_Real y, Standard_Real z) {
+gp_Trsf makeTranslation(Standard_Real x, Standard_Real y, Standard_Real z)
+{
   gp_Trsf trsf;
   trsf.SetTranslation(gp::Origin(), gp_Pnt(x, y, z));
   return trsf;
 }
 
-gp_Trsf makeScale(Standard_Real factor) {
+gp_Trsf makeScale(Standard_Real factor)
+{
   gp_Trsf trsf;
   trsf.SetScale(gp::Origin(), factor);
   return trsf;
 }
 
-TopoDS_Shape applyTransform(const TopoDS_Shape& aShape, const gp_Trsf& trsf) {
-  if (aShape.IsNull()) {
+TopoDS_Shape applyTransform(const TopoDS_Shape &aShape, const gp_Trsf &trsf)
+{
+  if (aShape.IsNull())
+  {
     return aShape;
   }
   BRepBuilderAPI_Transform theTransform(trsf);
@@ -111,18 +124,21 @@ TopoDS_Shape applyTransform(const TopoDS_Shape& aShape, const gp_Trsf& trsf) {
   return theTransform.Shape();
 }
 
-TopoDS_Shape makeUnion(std::vector<TopoDS_Shape> shapes) {
+TopoDS_Shape makeUnion(std::vector<TopoDS_Shape> shapes)
+{
   BRepAlgoAPI_BuilderAlgo aBuilder;
 
   TopTools_ListOfShape aLS;
-  for(TopoDS_Shape shape : shapes) {
+  for (TopoDS_Shape shape : shapes)
+  {
     aLS.Append(shape);
   }
 
   aBuilder.SetArguments(aLS);
 
-  aBuilder.Build(); 
-  if (aBuilder.HasErrors()) {
+  aBuilder.Build();
+  if (aBuilder.HasErrors())
+  {
     aBuilder.DumpErrors(Message::DefaultMessenger()->SendFail());
     TopoDS_Shape nullShape;
     return nullShape;
@@ -130,16 +146,21 @@ TopoDS_Shape makeUnion(std::vector<TopoDS_Shape> shapes) {
   return aBuilder.Shape();
 }
 
-TopoDS_Shape makeDifference(std::vector<TopoDS_Shape> shapes) {
+TopoDS_Shape makeDifference(std::vector<TopoDS_Shape> shapes)
+{
   BRepAlgoAPI_Cut aBuilder;
 
   TopTools_ListOfShape aLS;
   TopTools_ListOfShape aLT;
 
-  for(TopoDS_Shape shape : shapes) {
-    if (shape == shapes.front()) {
+  for (TopoDS_Shape shape : shapes)
+  {
+    if (shape == shapes.front())
+    {
       aLS.Append(shape);
-    } else {
+    }
+    else
+    {
       aLT.Append(shape);
     }
   }
@@ -148,7 +169,8 @@ TopoDS_Shape makeDifference(std::vector<TopoDS_Shape> shapes) {
   aBuilder.SetTools(aLT);
 
   aBuilder.Build();
-  if (aBuilder.HasErrors()) {
+  if (aBuilder.HasErrors())
+  {
     aBuilder.DumpErrors(Message::DefaultMessenger()->SendFail());
     TopoDS_Shape nullShape;
     return nullShape;
@@ -156,23 +178,29 @@ TopoDS_Shape makeDifference(std::vector<TopoDS_Shape> shapes) {
   return aBuilder.Shape();
 }
 
-TopoDS_Shape makeIntersection(std::vector<TopoDS_Shape> shapes) {
+TopoDS_Shape makeIntersection(std::vector<TopoDS_Shape> shapes)
+{
   TopTools_ListOfShape aLS;
   TopTools_ListOfShape aLT;
 
-  for (int i = 0; i < shapes.size(); i++) {
-    if (i == 0) {
+  for (int i = 0; i < shapes.size(); i++)
+  {
+    if (i == 0)
+    {
       aLS.Append(shapes.at(i));
-    } else {
+    }
+    else
+    {
       aLT.Append(shapes.at(i));
 
       BRepAlgoAPI_Common aBuilder;
       aBuilder.SetArguments(aLS);
       aBuilder.SetTools(aLT);
 
-      aBuilder.Build(); 
+      aBuilder.Build();
 
-      if (aBuilder.HasErrors()) {
+      if (aBuilder.HasErrors())
+      {
         aBuilder.DumpErrors(Message::DefaultMessenger()->SendFail());
         TopoDS_Shape nullShape;
         return nullShape;
@@ -194,20 +222,22 @@ extern "C" void onMainLoop()
   emscripten_cancel_main_loop();
 }
 
-
 int init()
 {
-  Message::DefaultMessenger()->Printers().First()->SetTraceLevel (Message_Trace);
-  Handle(Message_PrinterSystemLog) aJSConsolePrinter = new Message_PrinterSystemLog ("webgl-sample", Message_Trace);
-  Message::DefaultMessenger()->AddPrinter (aJSConsolePrinter); // open JavaScript console within the Browser to see this output
-  Message::DefaultMessenger()->Send (TCollection_AsciiString("NbLogicalProcessors: ") + OSD_Parallel::NbLogicalProcessors(), Message_Trace);
+  Message::DefaultMessenger()->Printers().First()->SetTraceLevel(Message_Trace);
+  Handle(Message_PrinterSystemLog) aJSConsolePrinter = new Message_PrinterSystemLog("webgl-sample", Message_Trace);
+  Message::DefaultMessenger()->AddPrinter(
+      aJSConsolePrinter); // open JavaScript console within the Browser to see this output
+  Message::DefaultMessenger()->Send(
+      TCollection_AsciiString("NbLogicalProcessors: ") + OSD_Parallel::NbLogicalProcessors(), Message_Trace);
 
-  // setup a dummy single-shot main loop callback just to shut up a useless Emscripten error message on calling eglSwapInterval()
-  emscripten_set_main_loop (onMainLoop, -1, 0);
+  // setup a dummy single-shot main loop callback just to shut up a useless Emscripten error message on calling
+  // eglSwapInterval()
+  emscripten_set_main_loop(onMainLoop, -1, 0);
 
   aViewer.run();
   aShapePrs = new AIS_Shape(makeBox(1, 1, 1));
-  Message::DefaultMessenger()->Send (OSD_MemInfo::PrintInfo(), Message_Trace);
+  Message::DefaultMessenger()->Send(OSD_MemInfo::PrintInfo(), Message_Trace);
 
   return 0;
 }
@@ -219,21 +249,18 @@ void init2()
 
 using namespace emscripten;
 
-EMSCRIPTEN_BINDINGS(react_cad) {
+EMSCRIPTEN_BINDINGS(react_cad)
+{
   function("init", &init);
   function("init2", &init2);
   class_<gp>("Space")
-    .class_function("Origin", &gp::Origin)
-    .class_function("OX", &gp::OX)
-    .class_function("OY", &gp::OY)
-    .class_function("OZ", &gp::OZ)
-  ;
+      .class_function("Origin", &gp::Origin)
+      .class_function("OX", &gp::OX)
+      .class_function("OY", &gp::OY)
+      .class_function("OZ", &gp::OZ);
   class_<gp_Pnt>("Point");
   class_<gp_Ax1>("Axis");
-  class_<TopoDS_Shape>("Shape")
-    .constructor<>()
-    .function("IsNull", &TopoDS_Shape::IsNull)
-  ;
+  class_<TopoDS_Shape>("Shape").constructor<>().function("IsNull", &TopoDS_Shape::IsNull);
   register_vector<TopoDS_Shape>("VectorShape");
   class_<gp_Trsf>("Transform");
   function("setShape", &setShape);
