a0b89b7eb19170bf574a05dbd9ff926da82d43ce
React CAD Dev <react.cad.dev@gmail.com>
2022-09-07 20:12:36 +0100

    fix(core): reduce initial memory

--- packages/core/dockerfile-changed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-git diff --name-only $(git describe --abbrev=0 HEAD^) | grep packages/core/Dockerfile
-# Grep returns an error if Dockerfile is unchanged, so this ony runs if it changes
-echo "Dockerfile changed"
